75a77defe50e71d74e8ba0cb9279d503069b3853
Sudharaka Palamakumbura <sudharakap@mun.ca>
2016-03-28 20:38:43 -0230

    implimented ssc partially, modified addEdge method

--- src/graph/CapGraph.java
+++ src/graph/CapGraph.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import util.GraphLoader;
 
@@ -17,11 +18,8 @@ import util.GraphLoader;
  */
 public class CapGraph implements Graph {
 
-	// Initialization of fields (number of vertices and edges)
-	private int numVertices;
-	private int numEdges;
-	
 	private Map<Integer, HashSet<Integer>> adjList = new HashMap<Integer, HashSet<Integer>>();
+	private List<Graph> stronglyConnected = new ArrayList<Graph>();
 	
 	/* (non-Javadoc)
 	 * @see graph.Graph#addVertex(int)
@@ -29,7 +27,6 @@ public class CapGraph implements Graph {
 	@Override
 	public void addVertex(int num) {
 		adjList.put(num, new HashSet<Integer>());
-		numVertices++;
 	}
 
 	/* (non-Javadoc)
@@ -37,8 +34,8 @@ public class CapGraph implements Graph {
 	 */
 	@Override
 	public void addEdge(int from, int to) {
-		adjList.get(from).add(to);
-		numEdges++;
+		if (this.exportGraph().containsKey(from) && this.exportGraph().containsKey(to))
+			adjList.get(from).add(to);
 	}
 
 	/* (non-Javadoc)
@@ -66,12 +63,20 @@ public class CapGraph implements Graph {
 	}
 
 	/* (non-Javadoc)
-	 * @see graph.Graph#getSCCs()
+	 * @see graph.Graph#getSCCs(): Returns all Strongly Connected Components (SSCs) 
+	 * of the graph.
 	 */
 	@Override
 	public List<Graph> getSCCs() {
-		// TODO Auto-generated method stub
-		return null;
+
+		Stack<Integer> finished = DFS(this.vertices(), false);
+		
+		CapGraph transpose = this.graphTanspose();
+		
+		transpose.DFS(finished, true);
+		System.out.println(this.stronglyConnected);
+		return stronglyConnected;
+		
 	}
 
 	/* (non-Javadoc)
@@ -80,6 +85,101 @@ public class CapGraph implements Graph {
 	@Override
 	public HashMap<Integer, HashSet<Integer>> exportGraph() {
 		return (HashMap<Integer, HashSet<Integer>>)adjList;
+	}
+	
+	/**
+	 * This is a helper method for the getSSC method
+	 * 
+	 * @param vertices the vertices of the graph as a Stack
+	 * @param sccList whether or not to add to the list of SCCs
+	 * @strongCon if this parameter is "true" then graphs (strongly connected components)
+	 * formed by the vertices are stored in "stronglyConnected"
+	 * @return Stack<Integer> 
+	 */
+	public Stack<Integer> DFS(Stack<Integer> vertices, boolean strongCon){
+		HashSet<Integer> visited = new HashSet<Integer>();
+		Stack<Integer> finished = new Stack<Integer>();
+		HashSet<Integer> scc = new HashSet<Integer>();
+		
+		while (!vertices.isEmpty()){
+			Integer v = vertices.pop();
+			
+			if (!visited.contains(v))
+				scc = DFSVisit(v, visited, finished);
+				
+			// piece together the strongly connected components, if strongCon == true
+			if (strongCon){
+				
+				CapGraph graph = new CapGraph();
+				// add all the vertices of the strongly connected components
+				for (Integer vertex: scc)
+					graph.addVertex(vertex);
+				
+				// add all the edges of the strongly connected components
+				for (Integer vertex: scc)
+					for (Integer a: adjList.get(vertex))
+						graph.addEdge(vertex, a);
+				
+				stronglyConnected.add(graph);
+			}			
+		}
+		return finished;
+	}
+	
+	/**
+	 * This is a helper method of BFS
+	 * 
+	 * @param vertex
+	 * @param visited 
+	 * @param finished
+	 */
+	public HashSet<Integer> DFSVisit(Integer vertex, HashSet<Integer> visited, Stack<Integer> finished){
+		
+		HashSet<Integer> strongConn = new HashSet<Integer>();
+		visited.add(vertex);
+		strongConn.add(vertex);
+		
+		for (Integer n: exportGraph().get(vertex))
+			if (!visited.contains(n))
+				DFSVisit(n, visited, finished);
+		
+		finished.push(vertex);
+		return strongConn;
+	}
+	
+	// returns the list of vertices of the graph
+	public Stack<Integer> vertices(){
+		Stack<Integer> vertices = new Stack<Integer>();
+		
+		// creating the list of vertices
+		for (Integer vertex: adjList.keySet()){
+			vertices.push(vertex);
+		}
+		
+		return vertices;
+	}
+	
+	/**
+	 * This method returns the transpose of the graph
+	 * 
+	 * @param graph
+	 * @return transpose of graph
+	 */
+	public CapGraph graphTanspose(){
+		
+		CapGraph transpose = new CapGraph();
+		
+		// copy over the vertices
+		for (Integer vertex: adjList.keySet())
+			transpose.addVertex(vertex);
+		
+		//copy the transpose of edges
+		for (Integer vertex: adjList.keySet()){
+			HashSet<Integer> edgeSet = adjList.get(vertex);
+			for (Integer v: edgeSet)
+				transpose.addEdge(v, vertex);
+		}
+		return transpose;
 	}
 	
 	public static void main(String[] args){
--- src/graph/CapGraphTest.java
+++ src/graph/CapGraphTest.java
@@ -2,6 +2,9 @@ package graph;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.Stack;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -44,23 +47,71 @@ public class CapGraphTest {
 
 	@Test
 	public void testGetEgonet() {
+		
+		// test for correctness of egonet centered at 2
 		Graph newgraph = testGraph.getEgonet(2);
-		assertEquals("Test egonet correctness", 4, newgraph.exportGraph().size());
-		assertEquals("Test egonet correctness", true, newgraph.exportGraph().get(2).contains(3));
-		assertEquals("Test egonet correctness", true, newgraph.exportGraph().get(2).contains(4));
-		assertEquals("Test egonet correctness", true, newgraph.exportGraph().get(2).contains(5));
-		assertEquals("Test egonet correctness", true, newgraph.exportGraph().get(3).contains(4));
-		assertEquals("Test egonet correctness", false, newgraph.exportGraph().get(3).contains(5));
+		assertEquals("Test for egonet of 2", 4, newgraph.exportGraph().size());
+		assertEquals("Test for egonet of 2", true, newgraph.exportGraph().get(2).contains(3));
+		assertEquals("Test for egonet of 2", true, newgraph.exportGraph().get(2).contains(4));
+		assertEquals("Test for egonet of 2", true, newgraph.exportGraph().get(2).contains(5));
+		assertEquals("Test for egonet of 2", true, newgraph.exportGraph().get(3).contains(4));
+		assertEquals("Test for egonet of 2", false, newgraph.exportGraph().get(3).contains(5));
+		
+		// test for empty graph when egonet vertex is not in the graph
+		Graph nullgraph = testGraph.getEgonet(10);
+		assertEquals("Test for empty egonet", 0, nullgraph.exportGraph().size());
+		
 	}
 
 	@Test
 	public void testGetSCCs() {
 		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testExportGraph() {
 		Integer[] test = {3,4};
 		assertArrayEquals("Check for correct edges", test, testGraph.exportGraph().get(1).toArray());
+	}
+	
+	@Test
+	public void testBFS(){
+		CapGraph testGraph = new CapGraph();
+		testGraph.addVertex(32);
+		testGraph.addVertex(50);
+		testGraph.addVertex(44);
+		testGraph.addVertex(25);
+		testGraph.addVertex(23);
+		testGraph.addVertex(65);
+		testGraph.addVertex(18);
+		
+		testGraph.addEdge(32, 50);
+		testGraph.addEdge(32, 44);
+		testGraph.addEdge(44, 50);
+		
+		testGraph.addEdge(25, 23);
+		testGraph.addEdge(25, 65);
+		testGraph.addEdge(25, 18);
+		
+		testGraph.addEdge(65, 23);
+		testGraph.addEdge(44, 18);
+		testGraph.addEdge(18, 23);
+		testGraph.addEdge(23, 25);
+		testGraph.addEdge(23, 18);
+		
+		assertEquals("Correct return size of BFS", 7, testGraph.DFS(testGraph.vertices(), false).size());
+		assertEquals("Correct return element of BFS", true, testGraph.DFS(testGraph.vertices(), false).contains(65));
+		assertEquals("Correct return element of BFS", true, testGraph.DFS(testGraph.vertices(), false).contains(23));
+		assertEquals("Correct return element of BFS", true, testGraph.DFS(testGraph.vertices(), false).contains(25));
+		assertEquals("Correct return element of BFS", true, testGraph.DFS(testGraph.vertices(), false).contains(50));
+		assertEquals("Correct return element of BFS", true, testGraph.DFS(testGraph.vertices(), false).contains(44));
+		assertEquals("Correct return element of BFS", true, testGraph.DFS(testGraph.vertices(), false).contains(18));
+		assertEquals("Correct return element of BFS", true, testGraph.DFS(testGraph.vertices(), false).contains(32));
+		
+		assertEquals("Correct return element of BFS", false, testGraph.DFS(testGraph.vertices(), false).contains(15));
+	
+		List<Graph> graphs = testGraph.getSCCs();
+		//System.out.println(testGraph.getSCCs());
 	}
 }
